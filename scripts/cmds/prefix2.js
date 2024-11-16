23072ecb2a77fe1d03462a5ac0f77286418770e1
Lord9s <nkunajunior399@gmail.com>
2024-11-16 22:48:41 +0200

    Update prefix2.js

--- scripts/cmds/prefix2.js
+++ scripts/cmds/prefix2.js
@@ -2,22 +2,22 @@ module.exports = {
  config: {
    name: "prefix",
    version: "1.0",
-   author: "Mr perfect",
+   author: "Lord king",
    countDown: 5,
    role: 0,
    shortDescription: "no prefix",
    longDescription: "no prefix",
-   category: "auto 🪐",
+   category: "𝑆𝑇𝑈𝑃𝐼𝐷",
  },
 
  onStart: async function(){}, 
  onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "prefix") {
+ if (event.body && event.body.toLowerCase() === "lord") {
  return message.reply({
  body: `
-Yo, my prefix is [  +  ]\n
+ 𝑆𝑈𝑃 𝑀𝑌 𝐺𝑈𝑌𝑆, 𝑆𝑂 𝑇𝐻𝐼𝑆 𝐼𝑆 𝑀𝑌 𝑃𝑅𝐸𝐹𝐼𝑋 𝑆𝑂 𝐼𝑁𝐽𝑂𝑌 𝑇𝐻𝐸 𝐽𝑈𝑁𝐼𝑂𝑅 𝐵𝑂𝑇[.] 𝑃𝐸𝐴𝐶𝐸\n
 `,
-  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/YMrKLFn.gif")
+  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/AubbSUX.mp4")
  });
  }
  }
