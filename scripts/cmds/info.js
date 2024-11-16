f91bce8a5c693884375bfa869114550ed4fc9d6a
Lord9s <nkunajunior399@gmail.com>
2024-11-16 14:14:12 +0200

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -15,14 +15,14 @@ module.exports = {
     envConfig: {}
   },
   onStart: async function ({ message }) {
-    const botName = "Mr perfect";
-    const botPrefix = "+";
-    const authorName = "Mr perfect";
-    const ownAge = "18";
+    const botName =  "🄻🄾🅁🄳𖣘𝘽𝙤𝙩࿐🔥 ";
+    const botPrefix = ".";
+    const authorName = "Lord king";
+    const ownAge = "Unknown";
     const teamName = "Github team";
-    const authorFB = "https://www.facebook.com/m.zenesha";
-    const authorInsta = "https://www.instagram.com/devildeacon?igsh=Z2R2dXZkYWNjanQw";
-    const tikTok = "https://www.tiktok.com/@rx______editz06?_t=8kK7a7zlktG&_r=1";
+    const authorFB = "https://m.me/lordjaydenSmith.1";
+    const authorInsta = "🤡";
+    const tikTok = "🙆";
     const urls = JSON.parse(fs.readFileSync('perfect.json'));
     const link = urls[Math.floor(Math.random() * urls.length)];
     const now = moment().tz('Asia/Kathmandu');
